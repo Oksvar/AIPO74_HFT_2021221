ebca984bc1147e18f6e1ccb51c267ef46cb872df
Petr <terminal8791fp@gmail.com>
2021-10-10 02:41:53 +0200

    Added Nuget packages and the mdf and ldf files in to Data

---
+++ AIPO74_HFT_2021221/AIPO74_HFT_2021221 Data/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIPO74_HFT_2021221_Data
+{
+    // Data Source =.\SQLEXPRESS;AttachDbFilename="|DataDirectory|\Database1.mdf";Integrated Security = True; User Instance = True
+    class Class1
+    {
+    }
+}
