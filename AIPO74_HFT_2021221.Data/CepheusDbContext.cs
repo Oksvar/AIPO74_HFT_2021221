ea43e6d570f2f3a1059d1cdb72dbe4b35448fcd6
Petr <terminal8791fp@gmail.com>
2021-10-17 15:46:39 +0200

    Create DbContext File

---
+++ AIPO74_HFT_2021221.Data/CepheusDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIPO74_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AIPO74_HFT_2021221.Data
+{
+     public  class CepheusDbContext 
+    {
+        
+    }
+}
