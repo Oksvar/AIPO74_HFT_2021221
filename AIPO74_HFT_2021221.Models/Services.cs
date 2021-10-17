32bcaa072c92c199b1e45cbde91386fa75db50d6
Petr <terminal8791fp@gmail.com>
2021-10-17 14:05:36 +0200

    Create Serveces file
    Which contain the type of services from Cepheus labor

---
+++ AIPO74_HFT_2021221.Models/Services.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIPO74_HFT_2021221.Models
+{
+   public class Services
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public int DevelopmentTime { get; set; }
+
+        [Range(1, 10)]
+        public int Dangerous { get; set; }
+    }
+}
