a410e381798c490eeb137510e9886a4cf17a5196
Petr <terminal8791fp@gmail.com>
2021-11-10 17:16:09 +0100

    Create a new Repositury class
    Create new a Repo class with Interface

---
+++ AIPO74_HFT_2021221.Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIPO74_HFT_2021221.Repository
+{
+    class Repository
+    {
+    }
+}
