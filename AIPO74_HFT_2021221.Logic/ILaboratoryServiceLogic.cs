6978d7aab91a6a3d6261b561879d7e52e8ced8d0
Petr <terminal8791fp@gmail.com>
2021-11-20 19:48:15 +0100

    Create laboratoryServiceLogic class

---
+++ AIPO74_HFT_2021221.Logic/ILaboratoryServiceLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIPO74_HFT_2021221.Models;
+
+namespace AIPO74_HFT_2021221.Logic
+{
+   public interface ILaboratoryServiceLogic
+    {
+        LaboratoryOrders GetOneOrder(int id);
+
+        Customer GetOneCustomer(int id);
+
+        IList<LaboratoryOrders> GetAllOrders();
+
+        IList<Customer> GetAllCustomers();
+
+        void ChangeSErvicePrice(int id, int newPrice);
+
+        void ChangeYearExpirience(int id, int yearXP);
+
+        void ChangePosition(int id, string newPosition);
+    }
+}
