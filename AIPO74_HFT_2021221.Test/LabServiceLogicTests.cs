322a3fccd936b9ad1a30de96e0de2668bd85524f
Petr <terminal8791fp@gmail.com>
2021-11-21 17:37:27 +0100

    Test Class

---
+++ AIPO74_HFT_2021221.Test/LabServiceLogicTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIPO74_HFT_2021221.Models;
+using AIPO74_HFT_2021221.Logic;
+using AIPO74_HFT_2021221.Repository;
+using Moq;
+
+namespace AIPO74_HFT_2021221.Test
+{
+    [TestFixture]
+    public class LabServiceLogicTests
+    {
+        private Mock<ICustomerRepo> customerRepo;
+        private Mock<ILaboratoryOrderRepo> LabOrderRepo;
+        private Mock<IServices> serviceRepo;
+        private Mock<IConnectionRepository> connectRepo;
+        private Mock<ILaboratoryStaffRepo> staffRepo;
+        private LaboratoryServiceLogic LaboratoryServiceLogic;
+
+        [Test]
+        public void FirstTest()
+        {
+
+        }
+        [Test]
+        public void SecondTest()
+        {
+
+        }
+        [Test]
+        public void ThirdTest()
+        {
+
+        }
+        [Test]
+        public void fourthTest()
+        {
+
+        }
+        [Test]
+        public void fifthTest()
+        {
+
+        }
+        [OneTimeSetUp]
+        public void MockLogic()
+        {
+
+        }
+    }
+}
